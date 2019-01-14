655ddada875132d1d7e852cd478199f87f224652
dustincheung <dustincnj@gmail.com>
2019-01-14 13:16:20 -0500

    prompt bar started

--- RGBgame.js
+++ RGBgame.js
@@ -1,8 +1,24 @@
+var colorArray = genRandomColors(6);
 var correctColor = pickColor();
-var colorArray = genRandomColors();
 var boxes = document.querySelectorAll(".box");
 var colorDisplay = document.getElementById("colorDisplay");
 var promptDisplay = document.getElementById("prompt");
+var h1 = document.querySelector("h1");
+
+function genRandomColors(num){
+	var tempArray = []
+
+	for(var i = 0; i < num; i++){
+		var red = Math.floor(Math.random()*256);
+		var green = Math.floor(Math.random()*256);
+		var blue = Math.floor(Math.random()*256);
+		var colorString = "rgb(" + red + ", " + green + ", " + blue + ")";
+
+		tempArray.push(colorString);
+	}
+
+	return tempArray;
+}
 
 function pickColor(){
 	var randNum = Math.floor(Math.random() * colorArray.length);
@@ -11,7 +27,7 @@ function pickColor(){
 
 function correctedColor(color){
 	for(var i = 0; boxes.length; i++){
-		boxes[i].style.backgroundColor = color;
+		boxes[i].style.background = color;
 	}
 }
 
@@ -25,7 +41,9 @@ for(var i = 0; i < boxes.length; i++){
 
 		if(selectColor === correctColor){
 			promptDisplay.textContent = "Correct!";
+			h1.style.backgroundColor = correctColor;
 			correctedColor(correctColor);
+
 		}else{
 			promptDisplay.textContent = "Try Again!";
 			this.style.backgroundColor = "#232323";
