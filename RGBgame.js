c8c6445866a8b4f822393f7094774c34c09ebae1
dustincheung <dustincnj@gmail.com>
2019-01-13 23:16:04 -0500

    basic js logic

--- RGBgame.js
+++ RGBgame.js
@@ -1,8 +1,29 @@
-var colors = [
-	"rgb(255, 0, 0,)",
-	"rgb(255, 255, 0,)",
-	"rgb(0, 255, 0,)",
-	"rgb(0, 255, 255,)",
-	"rgb(0, 0, 255,)",
-	"rgb(255, 0, 255,)",
+var colorArray = [
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)"
 ]
+
+var boxes = document.querySelectorAll(".box");
+var correctColor = colorArray[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = correctColor;
+
+for(var i = 0; i < boxes.length; i++){
+	boxes[i].style.backgroundColor = colorArray[i];
+
+	boxes[i].addEventListener("click",function(){
+		var selectColor = this.style.backgroundColor;
+
+		if(selectColor === correctColor){
+			alert("correct");
+		}else{
+			alert("wrong")
+		}
+	})
+}
+
